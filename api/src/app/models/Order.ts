5ff1e433bd884b05255cbabc385d300217109e29
Gerson Rocha <gersonrocha9@gmail.com>
2022-11-29 19:25:32 -0300

    feat: create models of MondoDB ✅

---
+++ api/src/app/models/Order.ts
@@ -0,0 +1,37 @@
+import { model, Schema } from "mongoose";
+
+export const Order = model(
+  "Order",
+  new Schema({
+    table: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["WAITING", "IN_PRODUCTION", "DONE"],
+      default: "WAITING",
+    },
+    createAt: {
+      type: Date,
+      default: Date.now, // sem os () pra ser gerado o timestamp no segundo que for criado
+      required: true,
+    },
+    products: {
+      required: true,
+      type: [
+        {
+          product: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: "Product",
+          },
+          quantity: {
+            type: Number,
+            default: 1,
+          },
+        },
+      ],
+    },
+  })
+);
